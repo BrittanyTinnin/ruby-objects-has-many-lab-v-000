aeab39111f162dd3200ace2a75a83692f8b57d03
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-04 03:32:43 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,42 @@
-require 'pry'
-
+# require 'pry'
+# 
+# class Artist
+# 
+#   attr_accessor :name
+# 
+#   @@song_count = 0
+# 
+#   def initialize(name)
+#     @name = name
+#     @songs = []
+#     @@song_count += 1
+# 
+#   end
+# 
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#     @@song_count += 1
+#   end
+# 
+#   def add_song_by_name(name)
+#     song = Song.new(name)
+#     @songs << song
+#     song.artist = self
+#     @@song_count += 1
+#   end
+# 
+#   def songs
+#     @songs
+#   end
+# 
+#   def self.song_count
+#     # binding.pry
+#     @@songs_count
+#   end
+# 
+# end
 class Artist
-
   attr_accessor :name
 
   @@song_count = 0
@@ -9,30 +44,26 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
-
   end
 
   def add_song(song)
-    @songs << song
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
+    self.songs << song
     song.artist = self
-    @@song_count += 1
+    @@song_count +=1
   end
 
   def songs
-    @songs
+    @songs 
   end
 
   def self.song_count
-    # binding.pry
-    @@songs_count
+    @@song_count
   end
-
 end
